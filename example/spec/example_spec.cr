99abf32adee626090b441d26ec415c07a9b3626c
Oleksii Fedorov <waterlink000@gmail.com>
2015-03-27 19:46:20 +0200

    Initial commit; Add example library

---
+++ example/spec/example_spec.cr
@@ -0,0 +1,7 @@
+require "./spec_helper"
+
+describe Example do
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ example/src/example.cr
@@ -0,0 +1,5 @@
+require "./example/*"
+
+module Example
+  # TODO Put your code here
+end
