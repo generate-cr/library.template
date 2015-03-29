0746db7de13624178dab753d10896892e50ff34b
Oleksii Fedorov <waterlink000@gmail.com>
2015-03-29 19:53:14 +0300

    Proper scoping under Generate module

---
+++ src/generate/library.cr
@@ -0,0 +1,7 @@
+require "./library/*"
+
+module Generate
+  module Library
+    # TODO Put your code here
+  end
+end
--- src/library.cr
+++
@@ -1,5 +0,0 @@
-require "./library/*"
-
-module Library
-  # TODO Put your code here
-end
