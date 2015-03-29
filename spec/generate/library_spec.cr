8af9d0dc9ad890c108fbe5c6c87eaaf084e5adc0
Oleksii Fedorov <waterlink000@gmail.com>
2015-03-29 19:46:58 +0300

    Namespace Library under Generate

---
+++ spec/generate/library_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+module Generate
+  describe Library do
+    it "works" do
+      false.should eq(true)
+    end
+  end
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/library"
+require "../src/generate/library"
