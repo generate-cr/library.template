f82c6fe9b0dad6c70e7cefa759ca32bc953bca95
Oleksii Fedorov <waterlink000@gmail.com>
2015-04-12 00:48:56 +0300

    Trying to make it actually work when it is a dependency

--- src/generate/library/view.cr
+++ src/generate/library/view.cr
@@ -18,19 +18,19 @@ module Generate
       end
     end
 
-    generate_template LibsKeepView, View, "src/template/.keep.ecr", "libs/.keep"
-    generate_template SpecKeepView, View, "src/template/.keep.ecr", "spec/#{config.name}/.keep"
+    generate_template LibsKeepView, View, "library.template", ".keep.ecr", "libs/.keep"
+    generate_template SpecKeepView, View, "library.template", ".keep.ecr", "spec/#{config.name}/.keep"
 
-    generate_template LicenseView, View, "src/template/LICENSE.ecr", "LICENSE"
-    generate_template GitignoreView, View, "src/template/.gitignore.ecr", ".gitignore"
-    generate_template ProjectfileView, View, "src/template/Projectfile.ecr", "Projectfile"
-    generate_template ReadmeView, View, "src/template/README.md.ecr", "README.md"
-    generate_template TravisView, View, "src/template/.travis.yml.ecr", ".travis.yml"
+    generate_template LicenseView, View, "library.template", "LICENSE.ecr", "LICENSE"
+    generate_template GitignoreView, View, "library.template", ".gitignore.ecr", ".gitignore"
+    generate_template ProjectfileView, View, "library.template", "Projectfile.ecr", "Projectfile"
+    generate_template ReadmeView, View, "library.template", "README.md.ecr", "README.md"
+    generate_template TravisView, View, "library.template", ".travis.yml.ecr", ".travis.yml"
 
-    generate_template SrcMainModuleView, View, "src/template/src/example.cr.ecr", "src/#{config.name}.cr"
-    generate_template VersionView, View, "src/template/src/example/version.cr.ecr", "src/#{config.name}/version.cr"
+    generate_template SrcMainModuleView, View, "library.template", "src/example.cr.ecr", "src/#{config.name}.cr"
+    generate_template VersionView, View, "library.template", "src/example/version.cr.ecr", "src/#{config.name}/version.cr"
 
-    generate_template SpecHelperView, View, "src/template/spec/spec_helper.cr.ecr", "spec/spec_helper.cr"
-    generate_template SpecMainModuleView, View, "src/template/spec/example_spec.cr.ecr", "spec/#{config.name}_spec.cr"
+    generate_template SpecHelperView, View, "library.template", "spec/spec_helper.cr.ecr", "spec/spec_helper.cr"
+    generate_template SpecMainModuleView, View, "library.template", "spec/example_spec.cr.ecr", "spec/#{config.name}_spec.cr"
   end
 end
